02174f495cebe9de87d0b705e2cd48d9a785c705
Julius-Okeyo <jaykopiyo@gmail.com>
2022-09-05 21:10:29 +0300

    Updated Lang_detector.py

--- Lang_Detector.py
+++ Lang_Detector.py
@@ -1,5 +1,6 @@
+import langdetect
 import os
-from langdetect import detect
 
-file = open(os.path(input('Path to file: ')))
-print(detect(file))
+file_path = os.path.abspath(input('Path to file: '))
+file = str(open(file_path))
+print(langdetect.detect(file))
